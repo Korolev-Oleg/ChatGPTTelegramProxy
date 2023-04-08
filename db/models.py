7490521d4244ee291744ad74a0524810198bb9eb
oleg <ok@hustn.cn>
2023-04-08 20:58:06 +0700

    fix tables

--- db/models.py
+++ db/models.py
@@ -10,18 +10,18 @@ class BaseModel(peewee.Model):
 
 
 class User(BaseModel):
-    username: peewee.CharField(max_length=256, unique=True)
-    first_name: peewee.CharField(max_length=256)
-    last_name: peewee.CharField(max_length=256)
-    access: peewee.BooleanField(default=False)
-    _raw_data_: peewee.TextField()
+    username = peewee.CharField(max_length=256, unique=True)
+    first_name = peewee.CharField(max_length=256)
+    last_name = peewee.CharField(max_length=256)
+    has_access = peewee.BooleanField(default=False)
+    _raw_data_ = peewee.TextField()
 
 
 class Message(BaseModel):
-    created_at: peewee.DateTimeField(default=datetime.now)
-    sender: User(backref="messages")
-    text: peewee.TextField()
-    gpt_response: peewee.TextField()
+    created_at = peewee.DateTimeField(default=datetime.now)
+    sender = User(backref="messages")
+    text = peewee.TextField()
+    gpt_response = peewee.TextField()
 
 
 def create_all():
