a763fde5384e407e193677cdd00976dccc6e1761
oleg <ok@hustn.cn>
2023-04-08 20:58:23 +0700

    update registration

--- services/login.py
+++ services/login.py
@@ -2,18 +2,23 @@ import telebot
 from db.models import User
 from loguru import logger
 
-AUTHENTICATED_USERS = ["@hustnct", "@sabushka77"]
+AUTHENTICATED_USERS = ["hustncn", "sabushka77"]
 
 
 def is_username_authenticate(username):
-    return username in AUTHENTICATED_USERS
+    ...
 
 
 def register_user(user: telebot.types.User):
-    new_user = User()
-    new_user.username = user.username
-    new_user.first_name = user.first_name
-    new_user.last_name = user.last_name
-    new_user._raw_data_ = user.to_json()
-    new_user.save()
-    logger.info('User {} registered!'.format(user.username))
+    try:
+        return User.get(User.username == user.username), False
+    except User.DoesNotExist:
+        new_user = User()
+        new_user.username = user.username
+        new_user.first_name = user.first_name
+        new_user.last_name = user.last_name
+        new_user._raw_data_ = user.to_json()
+        new_user.has_access = user.username in AUTHENTICATED_USERS
+        new_user.save()
+        logger.info("User {} registered!".format(user.username))
+        return new_user, True
